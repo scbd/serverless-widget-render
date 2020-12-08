3c352553a005d10baed67219aafd91fd9223ac83
Blaise Fonseca <blaise.fonseca@cbd.int>
2020-12-08 14:26:48 -0500

    install lint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  plugins: [
+    'align-assignments'
+  ],
+  extends: [
+    'scbd',
+  ],
+  // add your custom rules here
+  rules: {
+
+  }
+}
